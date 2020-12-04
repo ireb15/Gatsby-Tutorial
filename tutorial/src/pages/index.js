caa30e88bfa5674b86ad40b551343554f0be99fa
ireb15 <57650854+ireb15@users.noreply.github.com>
2020-12-04 19:51:33 +1300

    Learned how to set up linked components

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import {Link} from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Hello world!
+    <div>
+    <Link to="/blog/">blog page</Link>
+    </div>
+    <a href="https://www.gatsbyjs.org">gatsby docs</a>
+  </div>
 }
