338f572764258271395946dc8b3315dbbcffc1bf
ireb15 <57650854+ireb15@users.noreply.github.com>
2020-12-23 13:35:35 +1300

    Learn how to use CSS modules

--- tutorial/src/pages/blog.js
+++ tutorial/src/pages/blog.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styles from '../components/blog.module.css'
 
 const blog = () => {
     return (
         <Layout>
+          <div className={styles.page}>
           <h1>this is our blog page</h1>  
+          <p className={styles.text}>
+            kdshfjkbsdjhdbfgjkdfbgjbdfjkgbjklsdfbglbsfg fgbsdjbjb sdfghbjsdh  bhbbfjksdf gdfsb gjkhbsdfgb sfhjbs shjfgb jksf dghsbfghjssdhjkvbsdhbgks hsbfgjh sfhjg hjksdfbgsfhgjbskhfdgb 
+          </p>
+          </div>
         </Layout>
     )
 }
--- tutorial/src/pages/products.js
+++ tutorial/src/pages/products.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
+import styles from '../components/products.module.css'
+console.log(styles)
 
 export default class products extends Component {
     render() {
         return (
             <Layout>
+                <div className={styles.page}>
                 <h1>this is our products page</h1>
+                <p className={styles.text}>
+            kdshfjkbsdjhdbfgjkdfbgjbdfjkgbjklsdfbglbsfg fgbsdjbjb sdfghbjsdh  bhbbfjksdf gdfsb gjkhbsdfgb sfhjbs shjfgb jksf dghsbfghjssdhjkvbsdhbgks hsbfgjh sfhjg hjksdfbgsfhgjbskhfdgb 
+          </p>
+          </div>
             </Layout>
         )
     }
