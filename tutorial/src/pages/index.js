becc53beab48b90b0330ec6cbd797f4e4e365832
ireb15 <57650854+ireb15@users.noreply.github.com>
2020-12-23 13:15:37 +1300

    Learn how to use Global CSS styling

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -3,6 +3,8 @@ import Layout from '../components/Layout'
 
 export default function Home() {
   return <Layout>
-    <h1>hello from gatsby</h1>
+    <h1 style={{color:"red",textTransform:"uppercase"}}>hello from gatsby</h1>
+    <h1>hello world</h1>
+    <h1>hello people</h1>
   </Layout>
 }
